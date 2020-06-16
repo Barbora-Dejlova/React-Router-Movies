1899552d4f9c67b16feda7b0c23fceebc6da480f
Barbora Dejlova <barboradejlova@BarborasMacBook.fios-router.home>
2020-06-15 20:10:54 -0400

    done

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {useParams} from 'react-router-dom'
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
+  // console.log(props.key)
+  // const params = useParams();
+  const {id} = useParams()
+  
+  
   useEffect(() => {
-    const id = 1;
+    // const id = props.key
+    
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -19,12 +24,6 @@ const Movie = (props) => {
         });
 
   },[]);
-  
-  // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
